739423cff1da6d892c869d98945628ace0af429d
CampusSphere Admin <admin@campussphere.com>
2026-04-26 22:16:32 +0530

    Auto-register all models in admin panel

--- backend_login_system/admin.py
+++ backend_login_system/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+# Automatically register all models in this app so you can manage them in the Django Admin Panel
+app = apps.get_app_config('backend_login_system')
+
+for model_name, model in app.models.items():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
